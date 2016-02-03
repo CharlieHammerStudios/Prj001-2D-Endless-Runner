6405b1f2f02788d2f0bbee3ec1c9155eab6878b5
SpringDL <DanL4@live.ca>
2016-02-03 13:00:41 -0500

    Added player jumping

---
+++ 2D Endless Runner/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Universe universeSS;
+
+    public float jumpStr = 1500;
+
+    void Awake ()
+    {
+        universeSS = GameObject.FindGameObjectWithTag("Universe").GetComponent<Universe>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void FixedUpdate ()
+    {
+        if (universeSS.input_SpacePress)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpStr));
+            //Debug.Log("gooby");
+        }
+    }
+}
